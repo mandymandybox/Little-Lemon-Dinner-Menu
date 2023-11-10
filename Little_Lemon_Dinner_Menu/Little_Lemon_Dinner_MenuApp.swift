04945637190690fd2c0fec3777cde4329fff7117
Mandy7012 <alexwongfighting@gmail.com>
2023-11-10 23:26:48 +0800

    create new file

---
+++ Little_Lemon_Dinner_Menu/Little_Lemon_Dinner_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_Lemon_Dinner_MenuApp.swift
+//  Little_Lemon_Dinner_Menu
+//
+//  Created by Mandy Lee on 10/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Little_Lemon_Dinner_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
